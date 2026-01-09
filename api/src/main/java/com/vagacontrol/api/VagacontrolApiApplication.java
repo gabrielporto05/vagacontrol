881513bd3e716bc17f118566b7538e409370ab9a
Gabriel Porto <gpalmenara@gmail.com>
2026-01-09 10:07:41 -0300

    [Back End] feat: criação do projeto spring

---
+++ api/src/main/java/com/vagacontrol/api/VagacontrolApiApplication.java
@@ -0,0 +1,13 @@
+package com.vagacontrol.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagacontrolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagacontrolApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/vagacontrol/api/VagacontrolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagacontrol.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagacontrolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
