421d3705e2ad5fb7cb007daf0dd499e194125b9b
Gabriel Porto <gpalmenara@gmail.com>
2026-01-18 12:21:39 -0300

    [Back End] feat: add CORS configuration and update LoginResponse field name

--- api/src/main/java/com/vagacontrol/api/config/SecurityConfig.java
+++ api/src/main/java/com/vagacontrol/api/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configure(http))
+                .cors(cors -> cors.configurationSource(new CorsConfig().corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                 // rotas públicas
--- api/src/main/java/com/vagacontrol/api/dto/response/LoginResponse.java
+++ api/src/main/java/com/vagacontrol/api/dto/response/LoginResponse.java
@@ -2,6 +2,6 @@ package com.vagacontrol.api.dto.response;
 
 public record LoginResponse(
         String message,
-        String acess_token) {
+        String access_token) {
 
 }
